4b280ecf49df114047434c983f00fcdfed48f8fb
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-02-27 11:08:55 -0500

    Corregido ortografia y centrado el formulario de contacto

--- index.php
+++ index.php
@@ -203,7 +203,7 @@
                         </span>
                     </div>
                     <span>
-                        <span class="title-produts">MARKETING PROMOCIAL</span><br>
+                        <span class="title-produts">MARKETING PROMOCIONAL</span><br>
                         <small class="hidden-xs">Actividades promocionales - Activaciones - Btl</small>
                     </span>
                 </a>
@@ -706,45 +706,47 @@
           </div>
           <div class="contact-form wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
             <div class="row">
-              <div class="col-sm-6">
-                <form id="main-contact-form" name="contact-form" method="post" action="send.php">
-                  <div class="row  wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <input type="text" name="name" class="form-control" placeholder="Nombre" required="required">
-                      </div>
+                <div class="col-sm-3 hidden-xs"></div>
+                <div class="col-sm-6">
+                    <form id="main-contact-form" name="contact-form" method="post" action="send.php">
+                        <div class="row wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input type="text" name="name" class="form-control" placeholder="Nombre" required="required">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input type="text" name="phone" class="form-control" placeholder="Teléfono" required="required">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" class="form-control" placeholder="Correo" required="required">
+                        </div>
+                        <div class="form-group">
+                            <textarea name="message" id="message" class="form-control" rows="4" placeholder="Escriba su mensaje..." required="required"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- <div class="col-sm-6">
+                    <div class="contact-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+                      <ul class="address">
+                        <li><i class="fa fa-map-marker"></i> <span> Address:</span> 2400 South Avenue A </li>
+                        <li><i class="fa fa-phone"></i> <span> Phone:</span> +928 336 2000  </li>
+                        <li><i class="fa fa-envelope"></i> <span> Email:</span><a href="mailto:someone@yoursite.com"> support@oxygen.com</a></li>
+                        <li><i class="fa fa-globe"></i> <span> Website:</span> <a href="#">www.sitename.com</a></li>
+                      </ul>
                     </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <input type="text" name="phone" class="form-control" placeholder="Teléfono" required="required">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="Correo" required="required">
-                  </div>
-                  <div class="form-group">
-                    <textarea name="message" id="message" class="form-control" rows="4" placeholder="Escriba su mensaje..." required="required"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">Enviar</button>
-                  </div>
-                </form>   
-              </div>
-              <div class="col-sm-6">
-                <!-- <div class="contact-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
-                  <ul class="address">
-                    <li><i class="fa fa-map-marker"></i> <span> Address:</span> 2400 South Avenue A </li>
-                    <li><i class="fa fa-phone"></i> <span> Phone:</span> +928 336 2000  </li>
-                    <li><i class="fa fa-envelope"></i> <span> Email:</span><a href="mailto:someone@yoursite.com"> support@oxygen.com</a></li>
-                    <li><i class="fa fa-globe"></i> <span> Website:</span> <a href="#">www.sitename.com</a></li>
-                  </ul>
                 </div> -->
-              </div>
+                <div class="col-sm-3 hidden-xs"></div>
             </div>
           </div>
         </div>
-      </div>        
+      </div>
     </section>
 
     <footer id="footer">
